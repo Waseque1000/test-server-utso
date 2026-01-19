f5e6c1c20d388fccc9013cd6ec80e149dcf0b423
shahriar-u <shahriar5703@gmail.com>
2026-01-19 20:29:19 +0300

    mongodb setup

---
+++ index.js
@@ -0,0 +1,43 @@
+/** @format */
+
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://shahriar5703_db_user:iJ9T2PpmSdZjOMpW@cluster0.1ikjvvw.mongodb.net/?appName=Cluster0";
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const db = client.db("ArtifyDB");
+    const artworkCollection = db.collection("artworks");
+    const favoritesCollection = db.collection("favorites");
+
+    console.log("✅ Successfully connected to MongoDB!");
+
+
+    
